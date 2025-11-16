f87eae941318ce3ce84c157b4279567f4e0b03d7
AngelicaAa-ui <angelica11.11agudelo@gmail.com>
2025-11-16 18:41:48 -0500

    Correccion Final

--- js/procesarRegistro.js
+++ js/procesarRegistro.js
@@ -1,18 +1,18 @@
 function enviarFormulario(event) {
-            event.preventDefault(); 
-            var formData = new FormData(document.getElementById("formulario"));
+    event.preventDefault(); 
+    var formData = new FormData(document.getElementById("formulario"));
 
-            $.ajax({
-                url: "procesarRegistro.php",  
-                type: "POST",
-                data: formData,
-                processData: false,
-                contentType: false,
-                success: function(response) {
-                    $('#mensaje').html(response);
-                },
-                error: function() {
-                    $('#mensaje').html("<p style='color:red;'>Hubo un error al procesar el registro.</p>");
-                }
-            });
-        }
+        $.ajax({
+            url: "procesarRegistro.php",  
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                $('#mensaje').html(response);
+            },
+            error: function() {
+                $('#mensaje').html("<p style='color:red;'>Hubo un error al procesar el registro.</p>");
+            }
+        });
+    }
